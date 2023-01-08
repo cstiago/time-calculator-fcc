d79b6eb9a9e0b7b41025ab06213b721086a8eb9b
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-08 18:01:50 -0300

    feat(calculator): convert minutes

--- time_calculator.py
+++ time_calculator.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 def add_time(start, duration, day=''):
-    tomorrow = '(next day)'
-    n = 0
-    later = '(' + str(n) + ' days later'
-
-    days = (
+    am, pm= 'AM', 'PM'
+    days_of_week = (
         'Sunday',
         'Monday',
         'Tuesday',
@@ -14,11 +11,23 @@ def add_time(start, duration, day=''):
         'Friday',
         'Saturday'
     )
-
-    time = start.split()[0].split(':')
+    days = 0
+    
     period = start.split()[1]
-    duration = duration.split(':')
-    
+    time = [int(i) for i in start.split()[0].split(':')]
+    duration = [int(i) for i in duration.split(':')]
+
+    time[1] += duration[1]
+
+    if time[1] >= 60:
+        time[1] -= 60
+
+        if time[0] == 11 and period == pm:
+            days += 1
+            period = am
+
+        time[0] += 1
+
     new_time = ''
-    
+
     return new_time
