ff58e3e8ba4aef7778779d820f1c8e05504a8e5a
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-08 15:38:33 -0300

    refactor: clean files

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# This entrypoint file to be used in development. Start by reading README.md
+#!/usr/bin/env python3
+
 from time_calculator import add_time
 from unittest import main
 
+print(add_time('01:00 AM', '1:00', 'Sunday'))
 
-print(add_time("11:06 PM", "2:02"))
-
-
-# Run unit tests automatically
 main(module='test_module', exit=False)
