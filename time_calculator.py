fc5a336f6f8ba91d17e8a6939fda11ed10d0447e
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-08 16:37:10 -0300

    feat(calculator): split time elements

--- time_calculator.py
+++ time_calculator.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
-def add_time(start, duration):
+def add_time(start, duration, day=''):
+    tomorrow = '(next day)'
+    n = 0
+    later = '(' + str(n) + ' days later'
 
+    days = (
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    )
+
+    time = start.split()[0].split(':')
+    period = start.split()[1]
+    duration = duration.split(':')
+    
+    new_time = ''
+    
     return new_time
